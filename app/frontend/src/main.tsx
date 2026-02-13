6ed42a7b388f33c4ed4d284ca2c56f29db2c9f74
sam <samt@samt.st>
2026-02-13 16:28:41 +0700

    use a toast lib

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ToastProvider } from './contexts/ToastContext';
+import './index.css';
+import { Toaster } from 'sonner';
 import { DokployProvider } from './contexts/DokployContext';
 import { RefreshServicesProvider } from './contexts/RefreshServicesContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ToastProvider>
-      <DokployProvider>
-        <RefreshServicesProvider>
-          <App />
-        </RefreshServicesProvider>
-      </DokployProvider>
-    </ToastProvider>
+    <DokployProvider>
+      <RefreshServicesProvider>
+        <Toaster position="top-right" richColors />
+        <App />
+      </RefreshServicesProvider>
+    </DokployProvider>
   </React.StrictMode>
 );
 
